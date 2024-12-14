15811e03308bd323c33036e54aea5a91559251b5
Frédéric Metrich <frederic.metrich@live.fr>
2024-12-14 22:22:42 +0100

    Refactor inject_sketch_name.py for improved readability and consistency in subprocess calls

--- Mk2_fasterControl_Full/inject_sketch_name.py
+++ Mk2_fasterControl_Full/inject_sketch_name.py
@@ -1,33 +1,29 @@
-from datetime import datetime, timezone
+from datetime import datetime
 import os
-from pathlib import Path
 import subprocess
-from subprocess import check_output, CalledProcessError
+from subprocess import CalledProcessError
 
 def get_git_revision_short_hash() -> str:
-  try:
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'],shell=True).decode('utf-8').strip()
-  except CalledProcessError:
-    return "N/A"
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], shell=True).decode('utf-8').strip()
+    except CalledProcessError:
+        return "N/A"
 
 def get_git_current_branch() -> str:
-  try:  
-    return subprocess.check_output(['git', 'branch', '--show-current'],shell=True).decode('utf-8').strip()
-  except CalledProcessError:
-    return "N/A"
+    try:
+        return subprocess.check_output(['git', 'branch', '--show-current'], shell=True).decode('utf-8').strip()
+    except CalledProcessError:
+        return "N/A"
 
 Import("env")
-proj_path = env["PROJECT_DIR"]
+proj_path = os.path.join(env["PROJECT_DIR"], "dummy")
 
-proj_path = os.path.join(proj_path, "dummy")
-
-macro_value = r"\"" + os.path.split(os.path.dirname(proj_path))[1] + r"\""
-
+macro_value = f"\"{os.path.split(os.path.dirname(proj_path))[1]}\""
 tz_dt = datetime.now().replace(microsecond=0).astimezone().isoformat(' ')
 
 env.Append(CPPDEFINES=[
-  ("PROJECT_PATH", macro_value),
-  ("CURRENT_TIME", "\\\"" + tz_dt + "\\\""),
-  ("BRANCH_NAME", r"\"" + get_git_current_branch() + r"\""),
-  ("COMMIT_HASH", r"\"" + get_git_revision_short_hash() + r"\"")
+    ("PROJECT_PATH", macro_value),
+    ("CURRENT_TIME", f"\"{tz_dt}\""),
+    ("BRANCH_NAME", f"\"{get_git_current_branch()}\""),
+    ("COMMIT_HASH", f"\"{get_git_revision_short_hash()}\"")
 ])
