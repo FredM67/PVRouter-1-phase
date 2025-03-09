13d5f670bd2fe2428ec212fcce58ee3ce21d2df7
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-03-09 12:16:25 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-1-phase@5f8a65ebff86deed0b20f622bf8247d89b812b34 🚀

--- search/defines_6.js
+++ search/defines_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['screen_5faddress_0',['SCREEN_ADDRESS',['../utils__oled_8h.html#a4dc358585abdaa9531e340bab60ae0b5',1,'utils_oled.h']]],
-  ['serialprint_1',['SERIALPRINT',['../config_8h.html#a1e2bd592f53e63c8c0323f2f54483420',1,'config.h']]]
+  ['serialout_1',['SERIALOUT',['../config_8h.html#ad7346ce5b913655d3f18ecbfb791a332',1,'config.h']]],
+  ['serialprint_2',['SERIALPRINT',['../config_8h.html#a1e2bd592f53e63c8c0323f2f54483420',1,'config.h']]]
 ];
