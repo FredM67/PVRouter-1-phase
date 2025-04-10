cb50f788bd26c73a69f1b9dcab48e1d663c6b543
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-04-10 17:17:07 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-1-phase@e8169cf9551ea26cc21452abda78fb81a0b69535 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['declval_0',['declval',['../declval_8hpp.html#a4c9aed552e31ca560a00d61f804ee445',1,'declval.hpp']]],
   ['divmod10_1',['divmod10',['../FastDivision_8cpp.html#a23623280f6a9bc988eea84e057769267',1,'divmod10(uint32_t in, uint32_t &amp;div, uint8_t &amp;mod):&#160;FastDivision.cpp'],['../FastDivision_8h.html#a9d8e8674f8953bda0cf3dfd481d298fe',1,'divmod10(uint32_t in, uint32_t &amp;div, uint8_t &amp;mod) __attribute__((noinline)):&#160;FastDivision.cpp']]],
-  ['divu10_2',['divu10',['../FastDivision_8cpp.html#a14cd442deafa15661a264ec968edad00',1,'divu10(unsigned int n):&#160;FastDivision.cpp'],['../FastDivision_8h.html#a59251ab6913244a85145f233677cf977',1,'divu10(unsigned int n) __attribute__((noinline)):&#160;FastDivision.cpp']]],
+  ['divu10_2',['divu10',['../FastDivision_8cpp.html#a8533c8b2efd068df7dd1b2e4d65b7ced',1,'divu10(uint16_t n):&#160;FastDivision.cpp'],['../FastDivision_8h.html#a606046ac14bb90c70153682a6bd92419',1,'divu10(uint16_t n) __attribute__((noinline)):&#160;FastDivision.cpp']]],
   ['drawglyph_3',['drawGlyph',['../classu8x8__fake.html#a1a72eaefca6c07f7496e38ad9179aaf3',1,'u8x8_fake']]],
   ['drawstring_4',['drawString',['../classu8x8__fake.html#acdd3bde92e299eabe4cf372164f459c9',1,'u8x8_fake']]],
   ['drawtile_5',['drawTile',['../classu8x8__fake.html#ae71356971e7b6fe48334e53bc4513b65',1,'u8x8_fake']]]
