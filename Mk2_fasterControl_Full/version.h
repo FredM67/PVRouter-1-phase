1c0f1a6be522294f358d9133e113d8704539a855
Frédéric Metrich <frederic.metrich@live.fr>
2025-04-11 08:29:42 +0200

    Update version.h to reset CURRENT_TIME, BRANCH_NAME, and COMMIT_HASH to "N/A"

--- Mk2_fasterControl_Full/version.h
+++ Mk2_fasterControl_Full/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define PROJECT_PATH "Mk2_fasterControl_Full"
-#define CURRENT_TIME ""
-#define BRANCH_NAME "main"
-#define COMMIT_HASH ""
+#define CURRENT_TIME "N/A"
+#define BRANCH_NAME "N/A"
+#define COMMIT_HASH "N/A"
 
 #endif // VERSION_H
