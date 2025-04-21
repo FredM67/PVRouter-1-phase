45bd10df7e6fdf4551d9446a469a99f45621f544
FredM67 <45318189+FredM67@users.noreply.github.com>
2025-04-21 15:04:01 +0000

    Deploying to gh-pages from @ FredM67/PVRouter-1-phase@b8d013fdaef1581a3a139beae6f1292daf5a90ce 🚀

--- search/functions_9.js
+++ search/functions_9.js
@@ -4,8 +4,9 @@ var searchData=
   ['initializedisplay_1',['initializeDisplay',['../group__SegDisplay.html#ga3533a6f04780d5ed82d704bf39cebb69',1,'utils_display.h']]],
   ['initializedisplayhw_2',['initializeDisplayHW',['../group__SegDisplay.html#gada41f61abba371bab816bae257291f92',1,'utils_display.h']]],
   ['initializedisplaysw_3',['initializeDisplaySW',['../group__SegDisplay.html#ga41e6969691858ccbfcc03d15d9008c63',1,'utils_display.h']]],
-  ['initializeprocessing_4',['initializeProcessing',['../group__Initialization.html#gadeeaa08a0a50d8535fdf2c8a92d4aebc',1,'initializeProcessing():&#160;processing.cpp'],['../group__Initialization.html#gadeeaa08a0a50d8535fdf2c8a92d4aebc',1,'initializeProcessing():&#160;processing.cpp']]],
-  ['isr_5',['ISR',['../group__TimeCritical.html#ga05c2e5b588ced1cd7312f5b0edc5b295',1,'processing.cpp']]],
-  ['isrelayon_6',['isRelayON',['../classrelayOutput.html#ab6905bae7d7204139b695ca089defb20',1,'relayOutput']]],
-  ['ival_7',['ival',['../types_8h.html#a23913965d671d9cb03004085740837b9',1,'types.h']]]
+  ['initializeoldpcbpins_4',['initializeOldPCBPins',['../group__Initialization.html#gaee63eefc2be5271dc281e6554798a0ba',1,'initializeOldPCBPins():&#160;processing.cpp'],['../group__Initialization.html#gaee63eefc2be5271dc281e6554798a0ba',1,'initializeOldPCBPins():&#160;processing.cpp']]],
+  ['initializeprocessing_5',['initializeProcessing',['../group__Initialization.html#gadeeaa08a0a50d8535fdf2c8a92d4aebc',1,'initializeProcessing():&#160;processing.cpp'],['../group__Initialization.html#gadeeaa08a0a50d8535fdf2c8a92d4aebc',1,'initializeProcessing():&#160;processing.cpp']]],
+  ['isr_6',['ISR',['../group__TimeCritical.html#ga05c2e5b588ced1cd7312f5b0edc5b295',1,'processing.cpp']]],
+  ['isrelayon_7',['isRelayON',['../classrelayOutput.html#ab6905bae7d7204139b695ca089defb20',1,'relayOutput']]],
+  ['ival_8',['ival',['../types_8h.html#a23913965d671d9cb03004085740837b9',1,'types.h']]]
 ];
